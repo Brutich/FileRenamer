5df18ee7c968bd3d60332fdc289d6b5ae672f94c
Brutich <oleg87arh@gmail.com>
2019-04-01 02:33:12 +0300

    FileRenamer changed

--- Controls/FolderSelection.xaml.cs
+++ Controls/FolderSelection.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -26,6 +28,8 @@ namespace FileRenamer.Controls
             InitializeComponent();
         }
 
+        public string FolderPath { get; set; }
+
         private void OpenFolderDialog(object sender, RoutedEventArgs e)
         {
             WinForms.FolderBrowserDialog folderDialog = new WinForms.FolderBrowserDialog();
@@ -33,6 +37,81 @@ namespace FileRenamer.Controls
             folderDialog.SelectedPath = AppDomain.CurrentDomain.BaseDirectory;
             WinForms.DialogResult result = folderDialog.ShowDialog();
 
+            if(result == WinForms.DialogResult.OK)
+            {
+                this.FolderPath = folderDialog.SelectedPath;
+            }
         }
     }
+
+
+
+    /* WIP
+
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        private int _Clicks;
+
+        public int Clicks
+        {
+            get { return _Clicks; }
+
+            set
+            {
+                _Clicks = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public DelegateCommand ClickAdd
+        {
+            get
+            {
+                return new DelegateCommand((obj) => { Clicks++; }, (obj) => Clicks < 10);
+            }
+
+        }
+
+    }
+
+    class DelegateCommand : ICommand
+    {
+
+
+        private Action<object> execute;
+
+        private Func<object, bool> canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DelegateCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+
+    */
 }

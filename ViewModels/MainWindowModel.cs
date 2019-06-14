c35ad7991b4562cb445dc3e04f947abb65c187cc
Brutich <oleg87arh@gmail.com>
2019-06-15 00:28:00 +0300

    Add Expander

--- ViewModels/MainWindowModel.cs
+++ ViewModels/MainWindowModel.cs
@@ -220,8 +220,7 @@ namespace FileRenamer
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged([CallerMemberName]string prop = "")
         {
-            if (PropertyChanged != null)
-                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
     }
 
